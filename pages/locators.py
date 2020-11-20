a32e26049171c4db5eccaa081161a5322d189ba3
Taikom <moztaya@gmail.com>
2020-11-20 09:55:32 +0300

    перенос методов в basePage

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+#class MainPageLocators():
+
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -14,7 +14,11 @@ class ProductPageLocators():
     NAME_ON_PRODUCT_PAGE=(By.CSS_SELECTOR,".content h1")
     PRICE_ADDED_TO_BASKET_MESSAGE=(By.XPATH,'//div[@id="messages"]/div[3]/div//strong')
     PRICE_ON_PRODUCT_PAGE = (By.CSS_SELECTOR, ".product_main .price_color")
+    SUCCESS_MESSAGE =(By.CSS_SELECTOR,"#messages .alertinner")
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 
--- pages/product_page.py
+++ pages/product_page.py
@@ -17,3 +17,8 @@ class ProductPage(BasePage):
     def should_be_sum_correct(self):
         assert self.browser.find_element(*ProductPageLocators.PRICE_ADDED_TO_BASKET_MESSAGE).text == self.browser.find_element(*ProductPageLocators.PRICE_ON_PRODUCT_PAGE).text, "Price in message and in product page are not equal"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_message_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), "Message should disappeared, but didn't"
